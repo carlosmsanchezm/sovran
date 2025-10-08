3f8342c222737f0a06ab3262460ace1f2f0c92e1
carlosmsanchezm <carlos.sanchez.m.m@gmail.com>
2025-10-08 01:07:03 -0400

    Add branch coverage tests and CI workflow

--- aegis-vscode-remote/extension/__tests__/e2e/run-vscode-tests.js
+++ aegis-vscode-remote/extension/__tests__/e2e/run-vscode-tests.js
@@ -3,16 +3,19 @@ const { runTests } = require('@vscode/test-electron');
 
 async function main() {
   const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-  const extensionTestsPath = path.resolve(__dirname, './suite/index');
+  const extensionTestsPath = path.resolve(__dirname, './suite/run');
   const launchArgs = [
     '--disable-extensions',
     '--user-data-dir=' + path.join(__dirname, '.user-data'),
     '--skip-welcome',
     '--skip-release-notes',
   ];
-
+  
   process.env.AEGIS_TEST_PROXY_URL = process.env.AEGIS_TEST_PROXY_URL || 'https://127.0.0.1:7443';
   process.env.VSCODE_EXTENSION_TEST_BUNDLE_DIR = path.join(extensionDevelopmentPath, 'out-e2e');
+  process.env.MOCHA_REPORTER = process.env.MOCHA_REPORTER || 'mocha-junit-reporter';
+  process.env.MOCHA_FILE = process.env.MOCHA_FILE
+    || path.resolve(extensionDevelopmentPath, '__tests__/test-results/junit/mocha-e2e-results.xml');
 
   await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs });
 }
